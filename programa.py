7ba47a9e3732a5dfc8141acffeb645617e7c8529
Gabriel programmer <83989808+Gabriel-FerreirasSantos@users.noreply.github.com>
2021-08-28 21:29:27 -0300

    update programa; part9

--- programa.py
+++ programa.py
@@ -7,24 +7,31 @@ from jogos import games
 
 menu = 'MENU'
 
-
 função.linha()
 print(menu.center(26))
 função.linha()
-print(f'Loja Hardware em desenvolvimento')
+print(f'Loja Hardware em desenvolvimento  V0.1')
+print(f' ')
 print(f'[1] Calculadora')
 print(f'[2] Joguinhos')
 print(f'[3] Loja Hardware')
 print(f'[4] Suporte')
+função.linha()
 esc1 = input('')
+função.linha()
 if esc1 == '1':
+    calc = 'Calculadora'
+    print(calc.center(26))
+    função.linha()
     print(f'[1]Adição')
     print(f'[2]Subtração')
     print(f'[3]Divisão')
     print(f'[4]Mutiplicação')
     print(f'[5]Raiz quadrada')
     print(f'[6]Potenciação')
+    função.linha()
     esc2 = input('')
+    função.linha()
     if esc2 == '1':
         calculadora.contademais()
     elif esc2 == '2':
@@ -55,5 +62,11 @@ elif esc1 == '3':
 
 
 elif esc1 == '4':
-    função.linha2()
-    suporte.opções()    
+    
+    sup = 'Suporte'
+    print(sup.center(26))
+    função.linha()
+    suporte.opções()   
+
+else:
+    print(f'Não encontramos esse numero na lista')     
