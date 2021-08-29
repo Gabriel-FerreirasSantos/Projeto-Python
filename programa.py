2ab20bc0174007598b70d22bf3287984219fd22a
Gabriel programmer <83989808+Gabriel-FerreirasSantos@users.noreply.github.com>
2021-08-28 21:18:08 -0300

    update programa; part8

--- programa.py
+++ programa.py
@@ -11,19 +11,19 @@ menu = 'MENU'
 função.linha()
 print(menu.center(26))
 função.linha()
-print('Loja Hardware em desenvolvimento')
-print('[1] Calculadora')
-print('[2] Joguinhos')
-print('[3] Loja Hardware')
-print('[4] Suporte')
+print(f'Loja Hardware em desenvolvimento')
+print(f'[1] Calculadora')
+print(f'[2] Joguinhos')
+print(f'[3] Loja Hardware')
+print(f'[4] Suporte')
 esc1 = input('')
 if esc1 == '1':
-    print('[1]Adição')
-    print('[2]Subtração')
-    print('[3]Divisão')
-    print('[4]Mutiplicação')
-    print('[5]Raiz quadrada')
-    print('[6]Potenciação')
+    print(f'[1]Adição')
+    print(f'[2]Subtração')
+    print(f'[3]Divisão')
+    print(f'[4]Mutiplicação')
+    print(f'[5]Raiz quadrada')
+    print(f'[6]Potenciação')
     esc2 = input('')
     if esc2 == '1':
         calculadora.contademais()
@@ -38,10 +38,10 @@ if esc1 == '1':
     elif esc2 == '6':
         calculadora.contadepotenciação() 
     else:
-        print('Não encontramos essa opção')    
+        print(f'Não encontramos essa opção')    
 elif esc1 == '2':  
     função.fgamesss()
-    print('[1] pedra papel tesoura')
+    print(f'[1] pedra papel tesoura')
     escolha1 = input('')
     if escolha1 == '1':
         games.pedra()
